b02c21d4ca3049c8472dcc881020c1f1cf85eb63
RenatoNunes22 <renatonunes0011@hotmail.com>
2024-02-06 13:35:11 -0300

    adding and styling items in the app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,35 @@
 import { CategoryButton } from "@/components/categoryButton";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from "react";
+import { View, FlatList, SectionList, Text } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { Product } from "@/components/product";
 
 export default function Home() {
   const [categorySelected, setCategorySelected] = useState<string>(
     CATEGORIES[0]
   );
 
+  const sectionListRef = useRef<SectionList>(null);
+
   function handleCategorySelect(selectedCategory: string) {
     setCategorySelected(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory
+    );
+    if (sectionListRef.current)
+      sectionListRef.current.scrollToLocation({
+        sectionIndex,
+        itemIndex: 0,
+        animated: true,
+        viewOffset: 20,
+      });
   }
 
   return (
     <View className="bg-slate-900 flex-1 pt-8">
-      <Header title="Faca seu pedido" cartQuantity={3} />
+      <Header title="Faça seu pedido" cartQuantity={3} />
       <FlatList
         className="max-h-10 mt-4"
         data={CATEGORIES}
@@ -30,6 +44,21 @@ export default function Home() {
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+      <SectionList
+        ref={sectionListRef}
+        className="flex-1 px-5 mt-3"
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 10 }}
       />
     </View>
   );
